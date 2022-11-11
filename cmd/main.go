8bed53cb8a1353f6e70f22bd5707ee5bcf399e02
zxhoper <zxhoper@proton.me>
2022-11-11 14:10:58 -0800

    fix bugs on importing

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/zxhoper/dayOfWeek"
 )
 
 func main() {
-	fmt.Printf("\n\n 2022-11-11 is %s <br />\n\n", dayOfWeek.DayOfWeek(11, 11, 2022))
+	fmt.Printf("\n\n 2022-11-11 is %s <br />\n\n",dayOfWeek.DayOfWeek(11, 11, 2022))
 }
